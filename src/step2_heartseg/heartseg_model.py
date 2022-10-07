9a31692751c3aa2f05bec41c88c3b4105fc151fb
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2022-10-07 14:06:42 +0200

    update more print commands

--- src/step2_heartseg/heartseg_model.py
+++ src/step2_heartseg/heartseg_model.py
@@ -13,8 +13,8 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-import tensorflow
-tensorflow.compat.v1.logging.set_verbosity(tensorflow.compat.v1.logging.ERROR)
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from tensorflow.python.keras.utils import *
 from tensorflow.python.keras.engine import Input
