2d5e28f45aef83e934fa23829c21601b5cf72980
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2022-10-07 10:37:20 +0200

    add script to convert model weights for step1

---
+++ src/step1_conv_model_weights.py
@@ -0,0 +1,49 @@
+import os
+
+import h5py
+import numpy as np
+from tensorflow.keras.layers import Conv3D
+import yaml
+
+import step1_heartloc.heartloc_model as heartloc_model
+
+conf = "step1_heart_localization.yaml"
+base_conf_file_path = 'config/'
+
+conf_file_path = os.path.join(base_conf_file_path, conf)
+with open(conf_file_path) as f:
+  yaml_conf = yaml.load(f, Loader = yaml.FullLoader)
+
+down_steps = yaml_conf["model"]["down_steps"]
+model_input_size = yaml_conf["processing"]["model_input_size"]
+extended = yaml_conf["model"]["extended"]
+
+
+mgpu = 1
+crop_size = model_input_size
+input_shape = (crop_size, crop_size, crop_size, 1)
+model = heartloc_model.get_unet_3d(down_steps = down_steps, input_shape = input_shape, mgpu = mgpu, ext = extended)
+
+
+weights_file = "../data/step1_heartloc/model_weights/step1_heartloc_model_weights.hdf5"
+weights = h5py.File(weights_file, mode="r")
+weights = weights["model_1"]
+
+for layer in model.layers:
+    name = layer.name
+    print(f"Set weights of layer {name} ...", end="\r")
+    if name not in weights.keys():
+        print(f"Set weights of layer {name} ... skip because weights could not be found")
+        continue
+
+    if type(layer) != Conv3D:
+        print(f"Set weights of layer {name} ... skip because layers of type {type(layer)} are not supported")
+        continue
+
+    layer_weights = weights[name]
+
+    bias = np.array(layer_weights["bias:0"])
+    kernel = np.array(layer_weights["kernel:0"])
+    layer.set_weights([kernel, bias])
+
+model.save(weights_file, save_format="h5")
